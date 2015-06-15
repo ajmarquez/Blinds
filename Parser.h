ba1b0257f18a7b4831501577787711bb4b512b37
Abelardo J. Marquez <ajmarquezg@gmail.com>
2015-06-15 18:13:14 -0430

    Parser class creates the needed parse for the Blinds

---
+++ Parser.h
@@ -0,0 +1,13 @@
+//
+//  Parser.h
+//  AccordianView
+//
+//  Created by Abelardo Marquez on 6/15/15.
+//  Copyright (c) 2015 innofied.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parser : NSObject
+
+@end
---
+++ Parser.m
@@ -0,0 +1,13 @@
+//
+//  Parser.m
+//  AccordianView
+//
+//  Created by Abelardo Marquez on 6/15/15.
+//  Copyright (c) 2015 innofied.com. All rights reserved.
+//
+
+#import "Parser.h"
+
+@implementation Parser
+
+@end
